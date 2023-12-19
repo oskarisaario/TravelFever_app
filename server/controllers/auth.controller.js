bd6ddfe881e9ce2ef0328891372c52f949d20967
Oskari Saario <saario.oskari@gmail.com>
2023-12-19 16:02:26 +0200

    Created signin api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
@@ -10,6 +11,26 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json('User created successfully!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const signIn = async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const validUser = await User.findOne({ username });
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...userWithoutPassword } = validUser._doc;
+
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(userWithoutPassword);
+
   } catch (error) {
     next(error);
   }
