8e3070ca6bfa3e8449e23810e766c31885306cc9
Oskari Saario <saario.oskari@gmail.com>
2023-12-19 14:04:39 +0200

    Created user, pin and auth routers  and controllers

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import userRouter from './routes/user.route.js';
+import pinRouter from './routes/pin.route.js';
+import authRouter from './routes/auth.route.js';
+
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
   console.log('Connected to MongoDB');
@@ -12,8 +16,13 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
   }
 );
+
+app.use('/api/user', userRouter);
+app.use('/api/pin', pinRouter);
+app.use('/api/auth', authRouter);
