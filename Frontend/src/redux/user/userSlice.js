00b4058c58b625de34b262fd1b454f3577d8404f
Oskari Saario <saario.oskari@gmail.com>
2023-12-20 17:38:41 +0200

    Delete and signout user UI and api routes completed

--- Frontend/src/redux/user/userSlice.js
+++ Frontend/src/redux/user/userSlice.js
@@ -35,6 +35,30 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -44,7 +68,13 @@ export const {
   signinSuccess,
   updateUserStart,
   updateUserFailure,
-  updateUserSuccess } = userSlice.actions;
+  updateUserSuccess,
+  deleteUserStart,
+  deleteUserFailure,
+  deleteUserSuccess,
+  signOutUserStart,
+  signOutUserFailure,
+  signOutUserSuccess } = userSlice.actions;
 
 
 export default userSlice.reducer;
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -67,4 +67,14 @@ export const google = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
+};
+
+
+export const signout = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('You have logged out!');
+  } catch (error) {
+    next(error);
+  }
 }
